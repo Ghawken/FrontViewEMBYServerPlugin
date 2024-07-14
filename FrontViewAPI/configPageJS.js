a0aaba21ce56ee43f4a2b760b7f7e3c0fe3e174b
jsavell <contact@shadowater.com>
2024-07-14 11:19:14 -0500

    config ui updates for emby 4.8

---
+++ FrontViewAPI/configPageJS.js
@@ -0,0 +1,72 @@
+define(["loading", "emby-button", "emby-select"],
+    function (loading) {
+
+        return function(view) {
+            view.addEventListener('viewshow', () => {
+                const page = view;
+
+                loading.show();
+
+                var FrontViewConfigurationPage = {
+                    pluginUniqueId: "9574ac10-bf23-49bc-1111-924f23cfa48f"
+                };
+
+                const setDeviceList = function(config) {
+                    //Put the List of Devices in the Selection Box
+                    const deviceNameEl = page.querySelector("#deviceName");
+                    config.DevicesRunning.forEach(function (c) {
+                        let option = document.createElement("option");
+                        option.text = c.Client + '  : Running on:  ' + c.DeviceName + ' with User:  ' + c.DeviceId;
+                        option.value = c.Id;
+                        deviceNameEl.add(option);
+                    });
+                };
+
+                ApiClient.getPluginConfiguration(FrontViewConfigurationPage.pluginUniqueId).then(function (config) {
+                    setDeviceList(config);
+                    loading.hide();
+                });
+
+                const configFormSubmitListener = function(e) {
+                    e.stopPropagation();
+                    e.preventDefault();
+
+                    loading.show();
+                    const form = this;
+
+                    ApiClient.getPluginConfiguration(FrontViewConfigurationPage.pluginUniqueId).then(function (config) {
+                        config.SelectedDeviceId = form.querySelector("#deviceName").value; //$('#deviceName option:selected').val();
+                        ApiClient.updatePluginConfiguration(FrontViewConfigurationPage.pluginUniqueId, config).then(Dashboard.processPluginConfigurationUpdateResult);
+                    }).finally(function() {
+                        loading.hide();
+                    });
+
+                };
+
+                const refreshDeviceListListener = function(e) {
+                    e.stopPropagation();
+                    e.preventDefault();
+
+                    loading.show();
+
+                    const deviceNameEl = page.querySelector("#deviceName");
+                    deviceNameEl.options.length = 0;
+
+                    ApiClient.getPluginConfiguration(FrontViewConfigurationPage.pluginUniqueId).then(function (config) {
+                        setDeviceList(config);
+                    }).finally(function() {
+                        loading.hide();
+                    });
+
+                };
+
+                const configFormEl = page.querySelector(".FrontViewConfigurationForm");
+                configFormEl.addEventListener("submit", configFormSubmitListener, true);
+
+
+                const refreshBtnEl = page.querySelector("#refreshDeviceList");
+                refreshBtnEl.addEventListener("click", refreshDeviceListListener, true);
+            });
+        }
+    }
+);
